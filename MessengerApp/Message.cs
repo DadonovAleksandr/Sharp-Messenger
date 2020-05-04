024453bab4f0d15bf23a0ecdd5819463f246589e
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2020-05-04 14:46:43 +0300

    Added contacts screen, Message and User classes

---
+++ MessengerApp/Message.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessengerApp
+{
+	class Message
+	{
+		private string text;
+		private User sender;
+		private User reciever;
+
+		public Message(string text, User sender, User reciever)
+		{
+			this.text = text;
+			this.sender = sender;
+			this.reciever = reciever;
+		}
+
+		public string Text
+		{
+			get
+			{
+				return this.text;
+			}
+		}
+
+		public User Sender
+		{
+			get
+			{
+				return this.sender;
+			}
+		}
+
+		public User Reciever
+		{
+			get
+			{
+				return this.Reciever;
+			}
+		}
+	}
+}
